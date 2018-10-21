2eb9687718460681edfd1f1c5eb1865531c8fb8d
Marek Ciesla <ciesla.marek@gmail.com>
2018-10-21 15:54:28 +0200

    added actions on HomeServlet

--- web/src/main/java/pl/sdac/controller/HomeServlet.java
+++ web/src/main/java/pl/sdac/controller/HomeServlet.java
@@ -1,6 +1,7 @@
 package pl.sdac.controller;
 
 import pl.sdac.dto.BookDto;
+import pl.sdac.model.Action;
 import pl.sdac.service.BookService;
 
 import javax.servlet.ServletException;
@@ -15,29 +16,29 @@ import java.util.List;
 public class HomeServlet extends HttpServlet {
 
     BookService bookService = new BookService();
-//
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//
-////        Action action = Action.valueOf(request.getParameter("action"));
-////        Long bookId = Long.valueOf(request.getParameter("bookId"));
-////
-////        switch (action) {
-////            case ADD:
-////                response.sendRedirect("/AddBookServlet");
-////            case EDIT:
-////                response.sendRedirect("EditBookServlet?bookId=" + bookId);
-////            case SHOW:
-////                response.sendRedirect("/ShowBookDetailServlet?bookId=" + bookId);
-////                break;
-////            case DELETE:
-////                bookService.delete(bookId);
-////                response.sendRedirect("/HomeServlet");
-////                break;
-////            default:
-////                throw new UnsupportedOperationException("no action");
-////        }
-////    }
-//
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Action action = Action.valueOf(request.getParameter("action"));
+        Long bookId = Long.valueOf(request.getParameter("bookId"));
+
+        switch (action) {
+            case ADD:
+                response.sendRedirect("/AddBookServlet");
+            case EDIT:
+                response.sendRedirect("EditBookServlet?bookId=" + bookId);
+            case SHOW:
+                response.sendRedirect("/ShowBookDetailServlet?bookId=" + bookId);
+                break;
+            case DELETE:
+                bookService.delete(bookId);
+                response.sendRedirect("/HomeServlet");
+                break;
+            default:
+                throw new UnsupportedOperationException("no action");
+        }
+    }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         List<BookDto> books = bookService.findAll();
         request.setAttribute("books", books);
